98d20e13944563e23002bf2344c689ea5887dd2c
yuhu <nico.previtali@gmail.com>
2013-11-04 18:14:27 +0100

    Basic UI layout

--- vs-lukasbi-lamport/src/ch/ethz/inf/vs/android/lukasbi/lamport/MainActivity.java
+++ vs-lukasbi-lamport/src/ch/ethz/inf/vs/android/lukasbi/lamport/MainActivity.java
@@ -3,13 +3,36 @@ package ch.ethz.inf.vs.android.lukasbi.lamport;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	/**
+	 * Server settings
+	 */
+	private final String serverAddress = "vslab.inf.ethz.ch";
+	private final int serverPort = 5000;
+	
+	/**
+	 * UI elements
+	 */
+	private EditText name, message;
+	private Button register, send;
+	private TextView chat;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// reference UI elements
+		name = (EditText) findViewById(R.id.ipt_name);
+		message = (EditText) findViewById(R.id.ipt_message);
+		register = (Button) findViewById(R.id.btn_register);
+		send = (Button) findViewById(R.id.btn_send);
+		chat = (TextView) findViewById(R.id.txt_chat);
 	}
 
 	@Override
